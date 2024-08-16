78795608c613df4cfe1d20a8b50be1ac750674ea
ChefChilliSauce <navshukla30@gmail.com>
2024-08-16 23:36:06 +0530

    first commit

---
+++ Backend/Rubik/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void MakeCube()
+    {
+        char[][] top = new char[3][3];
+        char[][] bottom = new char[3][3];
+        char[][] left = new char[3][3];
+        char[][] right = new char[3][3];
+        char[][] front = new char[3][3];
+        char[][] back = new char[3][3];
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
