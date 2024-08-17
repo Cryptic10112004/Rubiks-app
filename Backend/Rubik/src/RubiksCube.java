6397cc66f68565a2bb9fc9bdfbec18410c198215
ChefChilliSauce <navshukla30@gmail.com>
2024-08-17 12:31:05 +0530

    Add cube state and display

--- Backend/Rubik/src/RubiksCube.java
+++ Backend/Rubik/src/RubiksCube.java
@@ -7,9 +7,63 @@ public class RubiksCube {
     private char[][] back = new char[3][3];     //Cube face array initiation Back: Blue
 
     private void initiateCube(){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                top[i][j] = 'Y';
+                bottom[i][j] = 'W';
+                left[i][j] = 'O';
+                right[i][j] = 'R';
+                front[i][j] = 'G';
+                back[i][j] = 'B';
+            }
+        }
 
     }
     public RubiksCube (){  // Method to access the initialized cube
         initiateCube();
+    }
+    public void DisplayCube(){
+        System.out.println("Face: Top");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(top[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Face: Bottom");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(bottom[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Face: Left");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(left[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Face: Right");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(right[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Face: Front");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(front[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Face: Back");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(back[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
